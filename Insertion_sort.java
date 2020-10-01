07f0b6dbb466b5751854bc014cb475f858aaf9f7
kathiroll <35343144+kathiroll@users.noreply.github.com>
2020-10-02 00:42:55 +0530

    Create Insertion_sort.java

---
+++ Insertion_sort.java
@@ -0,0 +1,23 @@
+/*
+Insertion Sort
+
+Insertion sort is a simple sorting algorithm that works similar to the way you sort playing cards in your hands. The array is virtually split into a sorted and an unsorted part. Values from the unsorted part are picked and placed at the correct position in the sorted part.
+
+*/
+void sort(int arr[])
+    {
+        int n = arr.length;
+        for (int i = 1; i < n; ++i) {
+            int key = arr[i];
+            int j = i - 1;
+  
+            /* Move elements of arr[0..i-1], that are
+               greater than key, to one position ahead
+               of their current position */
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+    }
